2f2442755e363c9f520d6b2f94fe4f41162e6267
matheusnff85 <matheus.marinhodsp@gmail.com>
2024-09-19 18:58:16 -0300

    feat: iniciando create page

---
+++ src/app/create/page.tsx
@@ -0,0 +1,10 @@
+import Header from "../components/header";
+
+export default function Create() {
+  return (
+    <section>
+      <Header />
+      <h1>Return the create page</h1>
+    </section>
+  );
+}
